76c64ae7c434d2b353946d18be0ba9273f661d98
29bucuk <pinargultekin@yahoo.com>
2019-07-27 23:56:16 -0400

    few changes  for appearance

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,7 +1,20 @@
 var friends = [
     {
-        name: "",
-        photo: "",
-        scores: []
+        name: "Jenn",
+        photo: "https://s3-ap-southeast-2.amazonaws.com/showandtellonline/wp/wp-content/uploads/2016/12/01084009/jennifer-aniston.jpg",
+        scores: [4,4,4,5,2,3,5,5,2,3]
+    },
+    {
+        name: "Julia",
+        photo: "https://m.media-amazon.com/images/M/MV5BMTQzNjU3MDczN15BMl5BanBnXkFtZTYwNzY2Njc4._V1_.jpg",
+        scores: [2,2,5,2,3,4,4,4,1,1]
+    },
+    {
+        name: "Javier",
+        photo: "https://upload.wikimedia.org/wikipedia/commons/thumb/7/7e/Javier_Bardem_Cannes_2018.jpg/220px-Javier_Bardem_Cannes_2018.jpg?imgurl=https%3A%2F%2Fanswersafrica.com%2Fwp-content%2Fuploads%2F2018%2F06%2FMahershala-Ali-640x640.jpg&imgrefurl=https%3A%2F%2Fanswersafrica.com%2Fmahershala-ali-wife-height-bio.html&docid=_7GhvxUwct444M&tbnid=oIoztMVE9EGjOM%3A&vet=10ahUKEwiHguGx-s7jAhXkYd8KHRwOB4cQMwhZKAUwBQ..i&w=640&h=640&bih=1005&biw=2048&q=ali%20mahershala&ved=0ahUKEwiHguGx-s7jAhXkYd8KHRwOB4cQMwhZKAUwBQ&iact=mrc&uact=8",
+        scores: [5,3,3,4,4,1,1,1,3,4]
     }
+
 ]
+
+module.exports = friends;
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,3 +1,17 @@
+//Dependencies
 
+var path = require("path");
+
+module.exports= function(app){
+
+//Routers
+app.get("/survey", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/survey.html"));
+});
+
+app.get("*", function(req, res){
+    res.sendFile(path.join(__dirname, "../public/home.html"));
+});
+}
 //A GET Route to `/survey` which should display the survey page.
 //A default, catch-all route that leads to `home.html` which displays the home page.
